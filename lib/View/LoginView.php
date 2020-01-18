f09f666a49807b9771020d04f15812965915c306
trainb <trainesben68@gmail.com>
2020-01-17 22:43:57 -0500

    styled login and register

--- lib/View/LoginView.php
+++ lib/View/LoginView.php
@@ -26,8 +26,6 @@ class LoginView extends View {
     }
 
     public function present() {
-        echo $this->head();
-
         echo '<div id="login">';
         echo $this->nav();
         echo $this->presentForm();
@@ -37,28 +35,30 @@ class LoginView extends View {
 
     public function presentForm() {
         return <<<HTML
-<form id="loginForm" method="post" action="post/login.php">
-    <fieldset>
-        <legend>LoginView</legend>
-
-        <p><label for="username">Username: </label>
-        <input type="text" id="username" name="username" placeholder="Username" autocomplete="Username" required></p>
-
-        <p><label for="password">Password: </label>
-        <input type="password" id="password" name="password" minlength="8" maxlength="20" placeholder="Password" autocomplete="current-password" required></p>
-
-        <p class="keep"><input type="checkbox" name="keep" id="keep">
-		<label for="keep">Keep me logged on</label></p>
-
-        <p><input type="submit" value="Log in"></p>
-        <p><a href="">Lost Password</a></p>
-
-        <p><a href="..">BT Glass HomeView</a></p>
-        <p><a href="./register.php">Become a Member</a></p>
-    </fieldset>
+<div class="login-wrapper">
+    <form id="loginForm" method="post" action="post/login.php">
+        <fieldset>
+            <legend>LoginView</legend>
     
-    <div class="message"></div>
-</form>
+            <p><label for="username">Username: </label>
+            <input type="text" id="username" name="username" placeholder="Username" autocomplete="Username" required></p>
+    
+            <p><label for="password">Password: </label>
+            <input type="password" id="password" name="password" minlength="8" maxlength="20" placeholder="Password" autocomplete="current-password" required></p>
+    
+            <p class="keep"><input type="checkbox" name="keep" id="keep">
+            <label for="keep">Keep me logged on</label></p>
+    
+            <p><input type="submit" value="Log in"></p>
+            <p><a href="">Lost Password</a></p>
+    
+            <p><a href="..">BT Glass HomeView</a></p>
+            <p><a href="./register.php">Become a Member</a></p>
+        </fieldset>
+        
+        <div class="message"></div>
+    </form>
+</div>
 HTML;
     }
 
--- login.php
+++ login.php
@@ -6,6 +6,9 @@ $view = new View\LoginView($site, $user, $_COOKIE);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+<?php echo $view->head(); ?>
+
+<body>
 <?php echo $view->present(); ?>
 
 </body>
